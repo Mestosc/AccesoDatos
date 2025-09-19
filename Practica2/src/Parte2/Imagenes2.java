fc66d952ea7f29b04e8a2fc638303e9d05044786
Oscar <oscarodriguez56@gmail.com>
2025-09-19 12:26:46 +0200

    Se puede notar el cambio en la foto que ahora ocupa más debido a añadir el nuevo archivo

---
+++ Practica2/src/Parte2/Imagenes2.java
@@ -0,0 +1,9 @@
+package Parte2;
+
+import static Parte1.Ejercicio2.*;
+
+public class Imagenes2 {
+    public static void main(String[] args) {
+        copiarArchivoByteByte("foto2.jpg",leerByteAByteFichero("marek-pavlik-YhpiG-0mKUg-unsplash.jpg"),true);
+    }
+}
