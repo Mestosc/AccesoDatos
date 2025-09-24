8b48e932e7002b2fa083eb98ea305f2b8b50c4a0
Oscar Rodriguez <orodriguezcabaleiro@danielcastelao.org>
2025-09-24 15:41:21 +0200

    Añadiendo diversas correcciones

--- Practica1JDBCAD/src/Anime.java
+++ Practica1JDBCAD/src/Anime.java
@@ -3,26 +3,25 @@ import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 public class Anime {
-    public void mostrarAnime(String nombre) {
+    public static void mostrarAnime(String nombre) {
         try (Connection conn = DBConnection.connect();
-            PreparedStatement stm = conn.prepareStatement("select * from anime where nombre = ?")) {
+            PreparedStatement stm = conn.prepareStatement("select * from anime where nome = ?")) {
             stm.setString(1, nombre);
             var rs = stm.executeQuery();
             while (rs.next()) {
-                System.out.println("ID: " + rs.getInt("id"));
-                System.out.println("Nombre: " + rs.getString("nombre"));
+                System.out.println("Nombre: " + rs.getString("nome"));
                 System.out.println("Descripcion: " + rs.getString("descripcion"));
+                System.out.println("Fecha: " + rs.getString("data"));
                 System.out.println("Puntuacion: " + rs.getInt("puntuacion"));
-                System.out.println("Fecha: " + rs.getString("fecha"));
             }
 
         } catch (SQLException e) {
             System.out.println("Error mostrando anime: " + e);
         }
     }
-    public void insertNuevoAnime(String nombre,String descripcion, int puntuacion, String fecha) {
+    public static void insertNuevoAnime(String nombre,String descripcion, int puntuacion, String fecha) {
         try (Connection conn = DBConnection.connect();
-             PreparedStatement statement = conn.prepareStatement("insert into anime (nombre, descripcion, puntuacion, fecha) values (?, ?, ?, ?)")) {
+             PreparedStatement statement = conn.prepareStatement("insert into anime (nome, descripcion, data, puntuacion) values (?, ?, ?, ?)")) {
             statement.setString(1, nombre);
             statement.setString(2, descripcion);
             statement.setInt(3, puntuacion);
--- Practica1JDBCAD/src/DBConnection.java
+++ Practica1JDBCAD/src/DBConnection.java
@@ -6,7 +6,9 @@ public class DBConnection {
     public static Connection connect() {
         Connection conn = null;
         try {
-            String url = "jdbc:postgresql://10.0.9.115:5432/probas";
+            String ipColegio = "10.0.9.115";
+            String ipCasa = "192.168.0.29";
+            String url = "jdbc:postgresql://"+ipCasa+"/probas";
             conn = DriverManager.getConnection(url, "postgres","admin");
         } catch (SQLException e) {
             System.out.println("Hola hay este error: " + e.getMessage());
