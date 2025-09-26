50468972707826c11a6faf8f7ab4340dd56931d2
Oscar Rodriguez <orodriguezcabaleiro@danielcastelao.org>
2025-09-26 12:49:57 +0200

    Haciendo cosas

--- Practica1JDBCAD/src/Main.java
+++ Practica1JDBCAD/src/Main.java
@@ -5,8 +5,8 @@ import java.util.ArrayList;
 public class Main {
     public static void main(String[] args) {
         Anime anime1 = Anime.obtenerAnime("Akira");
-        Anime.actualizarNombreDondeNombre("nome", "'Akira 2'", "Akira");
-        ArrayList<Anime> animes = Anime.obtenerAnimesNombre("Akira 2");
+        Anime.actualizarNombreDondeNombre("Akira", "Akira 2");
+        ArrayList<Anime> animes = Anime.obtenerAnimesNombre("Akira");
         if (animes!=null) {
         for (Anime anime : animes) {
             System.out.println(anime.nombre + " " + anime.descripcion + " " + anime.puntuacion + " " + anime.fecha);
